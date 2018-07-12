ee4b592363d30dc21050ecc7c263210fb8f55804
kels0la <samelsola@gmail.com>
2018-07-12 18:19:04 -0500

    Completed liri app

--- liri.js
+++ liri.js
@@ -1,65 +1,123 @@
 require("dotenv").config();
 
-const spotify = new Spotify(keys.spotify);
-const client = new Twitter(keys.twitter);
-/*Make it so liri.js can take in one of the following commands:
+const keys = require("./keys"); // could also be "./keys"
+const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
+const fs = require('fs')
+const OMDBrequest = require('request');
 
-* `my-tweets`
+const arg3 = process.argv.slice(3).join('+');
+runApp(process.argv[2], arg3);
 
-* `spotify-this-song`
+//  fs.writeFile("./log.txt", "Inception, Die Hard", function(err){
+// NOT SURE WHAT TO DO WITH THIS
+// })
 
-* `movie-this`
+function runApp(command, arg3) {
+    if (command === "movie-this" && arg3 != "") {
+        Movie(arg3);
+    }
+    else if (command === "movie-this" && arg3 === "") {
+        arg3 = "Mr+Nobody";
+        Movie(arg3);
+    }
+    else if (command === "my-tweets") {
+        Tweets(arg3);
+    }
+    else if (command === "spotify-this-song" && arg3 != "") {
+        Spotified(arg3);
+    }
+    else if (command === "spotify-this-song" && arg3 === "") {
+        arg3 = "Ace of Base: The Sign";
+        Spotified(arg3);
+    }
+    else if (command === "do-what-it-says") {
+        doIt(arg3);
+    }
+    else {
+        console.log("You have not specified the correct command \nTry: movie-this, spotify-this-song, my-tweets, or do-what-it-says");
+    }
+};
 
-* `do-what-it-says`
+function Movie(movieName) {
 
-node liri.js my-tweets
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-This will show your last 20 tweets and when they were created at in your terminal/bash window.
+    OMDBrequest(queryUrl, function (err, response, data) {
 
-node liri.js spotify-this-song '<song name here>'
+        if (!err && response.statusCode === 200) {
+            let obj = JSON.parse(data)
+            console.log('')
+            console.log("Title: " + obj.Title);
+            console.log("Release Year: " + obj.Year);
+            console.log("IMDB Rating: " + obj.imdbRating);
+            console.log("Rotten Tomatoes rating: " + obj.Ratings[1].Value);
+            console.log("Production Location: " + obj.Country);
+            console.log("Language: " + obj.Language);
+            console.log("Movie Plot: " + obj.Plot);
+            console.log("Main Actors: " + obj.Actors);
+        }
+        else {
+            return console.log("ERROR");
+        }
+    })
+};
 
-This will show the following information about the song in your terminal/bash window
+function Tweets() {
 
-Artist(s)
-The song's name
-A preview link of the song from Spotify
-The album that the song is from
+    const client = new Twitter(keys.twitter);
 
-If no song is provided then your program will default to "The Sign" by Ace of Base.
+    client.get('statuses/home_timeline', function (error, tweets, response) {//Could add parameters to only include my own tweets but it's more fun to look at others and view updates
 
-Client ID 0b17e93296f8477eb0e101bff7948201
-Client Secret d8fc5d23a9b244fe98c53d8825a0f217
+        if (!error) {
 
-node liri.js movie-this '<movie name here>'
+            for (let i = 0; i < tweets.length; i++) {
+                console.log([i + 1] + '. User name: ' + tweets[i].user.name);
+                console.log('Tweeted on: ' + tweets[i].created_at);
+                console.log("Tweet: " + tweets[i].text);
+                console.log("");
+            }
+        }
+        else {
+            console.log('Error occurred: ' + error);
+        }
 
+    })
+};
 
+function Spotified(title) {
 
+    const spotify = new Spotify(keys.spotify);
 
-This will output the following information to your terminal/bash window:
+    spotify.search({ type: 'track', query: title, limit: 1 }, function (error, data) {
 
-   * Title of the movie.
-   * Year the movie came out.
-   * IMDB Rating of the movie.
-   * Rotten Tomatoes Rating of the movie.
-   * Country where the movie was produced.
-   * Language of the movie.
-   * Plot of the movie.
-   * Actors in the movie.
+        result = data.tracks.items[0];
 
+        if (!error) {
+            console.log("");
+            console.log("Artist: " + result.album.artists[0].name);
+            console.log("Song name: " + result.name);
+            console.log("Album: " + result.album.name);
+            console.log("Link: " + result.external_urls.spotify);
+        }
+        else {
+            return console.log('Error occurred: ' + error);
+        }
 
-If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+    });
 
+};
 
-If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
+function doIt() {
+    
+    fs.readFile("random.txt", "utf8", function (error, data) {
 
-node liri.js do-what-it-says
+        if (error) {
+            return console.log(error);
+        }
 
-
-
-
-Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-
-It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-Feel free to change the text in that document to test out the feature for other commands.
-*/
+        var dataArr = data.split(",");
+        console.log(dataArr);
+        runApp(dataArr[0], dataArr[1]);
+    });
+};
